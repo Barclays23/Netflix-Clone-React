6bd7ee21034778049ba2d8d669b35061051efcbe
Barclays23 <itsmesajeer@gmail.com>
2025-05-02 16:40:40 +0530

    some checks on env and firebase console

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,9 @@
-// src/services/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getStorage } from "firebase/storage";
+
+
 
 
 const firebaseConfig = {
